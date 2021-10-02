41bf8cda25b6ae97a3801e3c6df24afd9cf4afb8
Berna POLAT <73580153+naberna@users.noreply.github.com>
2021-10-02 09:59:45 +0200

    login and register

--- js/scripts.js
+++ js/scripts.js
@@ -100,3 +100,43 @@ $(function () {
       $('[data-tabs]').css('min-height', height + 40 + 'px');
  
 }(jQuery, document));
+
+// Get the modal
+var modal = document.getElementById("loginModal");
+var modal2 = document.getElementById("registerModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("login");
+var btn2 = document.getElementById("register");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close")[0];
+var span2 = document.getElementById("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+    if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
+  }
