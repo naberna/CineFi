b9498ebebdc8ab8b285f05a4a433b2b31ae0a189
Berna POLAT <73580153+naberna@users.noreply.github.com>
2021-10-04 15:45:45 +0200

    connec-wallet added

--- js/scripts.js
+++ js/scripts.js
@@ -110,16 +110,19 @@ $(".progress-bar").each(function () {
 var modal = document.getElementById("loginModal");
 var modal2 = document.getElementById("registerModal");
 var modal3 = document.getElementById("forgetPasswordModal");
+var modal4 = document.getElementById("connectWalletModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("login");
 var btn2 = document.getElementById("register");
 var btn3 = document.getElementById("forgetPassword");
+var btn4 = document.getElementById("connectWallet");
 
 // Get the <span> element that closes the modal
 var span = document.getElementById("close");
 var span2 = document.getElementById("close2");
 var span3 = document.getElementById("close3");
+var span4 = document.getElementById("close4");
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
@@ -131,6 +134,9 @@ btn2.onclick = function () {
 btn3.onclick = function () {
   modal3.style.display = "block";
 };
+btn4.onclick = function () {
+  modal4.style.display = "block";
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
@@ -141,6 +147,9 @@ span2.onclick = function () {
 };
 span3.onclick = function () {
   modal3.style.display = "none";
+};
+span4.onclick = function () {
+  modal4.style.display = "none";
 };
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -156,6 +165,11 @@ window.onclick = function (event) {
 window.onclick = function (event) {
   if (event.target == modal3) {
     modal3.style.display = "none";
+  }
+};
+window.onclick = function (event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
   }
 };
 
